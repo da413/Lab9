b51814cb9ce272ebbbafc43eda7b2d935b5163cb
da413 <danielwolfcol@gmail.com>
2025-10-17 13:50:47 -0400

    more scene set up

---
+++ Assets/Daniel/Scripts/ScoreKeeper.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    public int score;
+
+    void Awake()
+    {
+        score = 0;
+    }
+}
