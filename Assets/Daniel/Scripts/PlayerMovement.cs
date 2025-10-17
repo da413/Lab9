07a01985c3151e220d4a38e0c945f34bbdb143bd
da413 <danielwolfcol@gmail.com>
2025-10-17 11:54:33 -0400

    animator walking set up in player movement script

--- Assets/Daniel/Scripts/PlayerMovement.cs
+++ Assets/Daniel/Scripts/PlayerMovement.cs
@@ -6,8 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float playerSpeed;
     float horizontal;
+    Animator animator; 
     
-
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,5 +19,12 @@ public class PlayerMovement : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
 
         transform.Translate(new Vector3((horizontal*playerSpeed*Time.deltaTime), 0, 0));
+        if(horizontal != 0){
+            animator.SetBool("IsWalking", true);
+        }
+        else{
+            animator.SetBool("IsWalking", false);
+        }
+        
     }
 }
